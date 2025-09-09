cdb5cf4e8c4e90b013426eaf473c75257bb3a3a5
24DP2MMeln <A240532mm@rvt.lv>
2025-09-09 13:35:12 +0300

    Message

---
+++ src/main/java/rvt/App.java
@@ -0,0 +1,15 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+
+      System.out.println("Write a message: ");
+
+      String message = scanner.nextLine();
+
+      System.out.println(message);
+}
+}
