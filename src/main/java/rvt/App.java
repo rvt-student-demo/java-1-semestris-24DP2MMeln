4fb99b754b083e4cc1682b304382b910a8d169e6
24DP2MMeln <A240532mm@rvt.lv>
2025-09-18 09:08:28 +0300

    Programma, kas izvadīts atzīmi

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,12 +1,27 @@
 package rvt;
 
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
 
-      //Your code here!
+      Scanner scanner = new Scanner(System.in);
+
+      System.out.println("Enter any number");
+      int number = scanner.nextInt();
+
+      if (number > 0) {
+        System.out.println("Number is positive: " + number);
+      }
+      else if (number == 0) {
+        System.out.println("The number is zero!");
+      }
+      else {
+        System.out.println("The number is negative: " + number);
+      }
 
 
+     
     
 }
 }
--- src/main/java/rvt/VariousVariable.java
+++ src/main/java/rvt/VariousVariable.java
@@ -1,14 +1,7 @@
 package rvt;
-
 public class VariousVariable {
     
     public static void main(String[] args) {
-
-      int chickenAmount = 9000;
-      double bacon_kg = 0.1;
-      String tractor = "Zetor";
-      System.out.println("Chicken: \n" + chickenAmount);
-      System.out.println("Bacon (kg):\n" + bacon_kg);
-      System.out.println("Tractor: \n" + tractor);
+   
     }
 }
