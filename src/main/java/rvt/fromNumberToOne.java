b38135d2176ed39e1cfb81eacd6b66a8466c4a4b
24DP2MMeln <A240532mm@rvt.lv>
2025-10-31 13:20:55 +0200

    From number to one

---
+++ src/main/java/rvt/fromNumberToOne.java
@@ -0,0 +1,24 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class fromNumberToOne {
+    public static void main(String[] args) {
+
+     Scanner scanner = new Scanner(System.in);
+     printFromNumberToOne(2);
+      }
+     
+     public static void printFromNumberToOne(int number) {
+        for (int i = 0; i < number; number--) {
+            System.out.println(number);
+        }
+     
+     }
+     
+
+     
+
+     
+
+    } 
